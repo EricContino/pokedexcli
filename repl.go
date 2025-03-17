303d0792b425bedb8ef6488075c4255d191ce687
Eric Contino <evcontino5107@gmail.com>
2025-03-16 21:13:37 -0400

    CH1L4 initial repl loop

--- repl.go
+++ repl.go
@@ -1,7 +1,27 @@
 package main
 
-import "strings"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
 
 func cleanInput(text string) []string {
     return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+    scanner := bufio.NewScanner(os.Stdin)
+    for {
+        fmt.Print("Pokedex> ")
+        scanner.Scan()
+        input := scanner.Text()
+        clean := cleanInput(input)
+        if len(clean) == 0 {
+            continue
+        }
+        commandName := clean[0]
+        fmt.Printf("Your command was: %s\n", commandName)
+    }
 }
